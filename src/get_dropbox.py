b90dd98b1f3f88a787fcc4e89aefefb1294eeb3d
Hulai Zhang <hulaizh@gmail.com>
2020-04-01 15:01:45 +0800

    add google and dropbox python scripts

---
+++ src/get_dropbox.py
@@ -0,0 +1,26 @@
+import urllib.request
+
+
+def get_from_dropbox(url, destination):
+
+    if url.endswith('dl=1'):
+        URL = url
+    elif url.endswith('dl=0'):
+        URL = url.replace('dl=0', 'dl=1')
+    else:
+        URL = False
+        print('File link is wrong! Please copy the right link.')
+
+    if URL:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        response.close()
+
+        with open(destination, 'wb') as f:
+            f.write(data)
+
+
+if __name__ == "__main__":
+    url = "https://www.dropbox.com/s/221s9p8p8fap61x/Bryan%20%20Everett.xlsx?dl=0"
+    destination = '../output/dropbox_sample.csv'
+    get_from_dropbox(url, destination)
